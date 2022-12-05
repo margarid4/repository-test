b96a5a028a0d37f2d25409a333da405db2a7c19c
margarid4 <a45259@alunos.ipb.pt>
2022-12-05 23:53:09 +0000

    simple plan

--- IA game T2/Assets/GAction.cs
+++ IA game T2/Assets/GAction.cs
@@ -8,7 +8,7 @@ public abstract class GAction : MonoBehaviour
     public string actionName = "Action";
     public float cost = 1.0f;
     public GameObject target;
-    public GameObject targetTag;
+    public string targetTag;
     public float duration = 0;
     public WorldStates[] preConditions;
     public WorldStates[] afterEffects;
